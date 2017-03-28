47c91381d15f2501cc5825c5af60a5399dad7fa4
Binh Nguyen <binhnguyen.plk91@gmail.com>
2017-03-28 21:53:17 +0700

    Seprate code to code blocks
    Make code easier to read by seprating it to many function

--- src/selectionsort/SelectionSort.java
+++ src/selectionsort/SelectionSort.java
@@ -8,28 +8,43 @@ import java.util.Scanner;
  */
 public class SelectionSort {
 
-    public static void main(String[] args) {
+    int[] value = new int[5];
+    int i = 0;
+    int j = 0;
+
+    void Input() {
         int i;
         Scanner sc = new Scanner(System.in);
-        int[] value = new int[5];
         System.out.println("Enter 5 number:");
-        for (i = 0; i < 5; i++) {            
+        for (i = 0; i < 5; i++) {
             value[i] = sc.nextInt();
         }
+    }
+
+    void OutPut() {
+        System.out.println("After selection sort:");
+        for (int i = 0; i < 5; i++) {
+            System.out.print(+value[i] + " ");
+        }
+    }
+
+    void Processing() {
         int temp = 0;
-        for (int j = 0; j < 4; j++) {
+        for (j = 0; j < 4; j++) {
             for (i = j + 1; i < 5; i++) {
                 if (value[j] > value[i]) {
-
                     temp = value[j];
                     value[j] = value[i];
                     value[i] = temp;
                 }
             }
         }
-        System.out.println("After selection sort:");
-        for (i = 0; i < 5; i++) {
-            System.out.print(+value[i] + " ");
-        }
+    }
+
+    public static void main(String[] args) {
+        SelectionSort ss = new SelectionSort();
+        ss.Input();
+        ss.Processing();
+        ss.OutPut();
     }
 }
